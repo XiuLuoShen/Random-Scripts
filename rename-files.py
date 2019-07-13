31f86b4347b6939154e1cb1c398a56a2ae6d5b09
XiuLuoShen <zengzeng419@gmail.com>
2019-07-13 01:57:04 -0400

    Renaming Files script
    Motivation: Mass rename files for the purpose of labelling for deep-learning.

---
+++ rename-files.py
@@ -0,0 +1,44 @@
+#! python3
+# rename-files.py - Renames files according in the format of "name-#", where name uses snake or camel case
+
+import shutil, re, os, argparse
+
+def main(newFileNameLabel, dir):
+    allFilesNames = set(os.listdir(dir))
+    fileNameRegex = re.compile(r'(.+)-(\d+)(\.[a-zA-Z]+)$')   # file name is in the format of: name-#.(file extension)
+    excludedNumbers = set()
+
+    for file in os.listdir(dir):
+        if not os.path.isfile(file):
+            allFilesNames.remove(file)
+        checkName = fileNameRegex.fullmatch(file)
+        if checkName is not None:
+            if int(checkName.group(2)) < len(allFilesNames):
+                excludedNumbers.add(int(checkName.group(2)))
+                print("Skipping file: %s" % file)
+                allFilesNames.remove(file)
+
+    number = 0
+    renamedFiles = 0
+    for file in allFilesNames:
+        while number in excludedNumbers:
+            number = number+1
+        newName = newFileNameLabel + "-" + str(number) + os.path.splitext(file)[1];
+        number = number+1
+        # print("Renaming file: " + file + " to " + newName)
+        renamedFiles = renamedFiles + 1
+        shutil.move(file, newName)
+
+    print("%d files were renamed!" % renamedFiles)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process arguments to rename files')
+    parser.add_argument('--filename', dest='fileNameLabel', required=True, help='name/labels for files')
+    parser.add_argument('--dir', dest='directory', required=True, help='directory in which files should be renamed')
+    args = parser.parse_args()
+    print(f'Attempting to rename files in: {args.directory:s} to label {args.fileNameLabel:s} ')
+    if  not (os.path.abspath(args.directory).startswith("V")):
+        print("Trying to rename files in disk other than V, aborting!")
+        quit()
+    else:
+        main(args.fileNameLabel, args.directory)
